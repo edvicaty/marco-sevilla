47378a1156fbe4c3057cb24944ab68662e9e2cc3
edvicaty <edvicaty@hotmail.com>
2022-03-23 09:35:47 -0500

    add eslint

--- packages/gatsby-theme-integrations/src/components/seo.js
+++ packages/gatsby-theme-integrations/src/components/seo.js
@@ -82,6 +82,7 @@ Seo.defaultProps = {
 Seo.propTypes = {
   description: PropTypes.string,
   lang: PropTypes.string,
+  // eslint-disable-next-line react/forbid-prop-types
   meta: PropTypes.arrayOf(PropTypes.object),
   title: PropTypes.string.isRequired,
 }
--- packages/gatsby-theme-integrations/src/gatsby-plugin-theme-ui/index.js
+++ packages/gatsby-theme-integrations/src/gatsby-plugin-theme-ui/index.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import baseTheme from "gatsby-ui/src/gatsby-plugin-theme-ui"
 
 export default baseTheme
