34666fbe9390e0be1e9c2551d2e9c047542a0f3f
edvicaty <edvicaty@hotmail.com>
2022-04-05 21:38:14 -0500

    add contentful

--- packages/gatsby-theme-integrations/gatsby-config.js
+++ packages/gatsby-theme-integrations/gatsby-config.js
@@ -1,3 +1,5 @@
+require("dotenv").config()
+
 module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,6 +26,14 @@ module.exports = {
         // theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `${__dirname}/src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `ba413gd9v6h5`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
